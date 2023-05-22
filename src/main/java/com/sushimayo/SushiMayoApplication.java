0885cfaf94e2b27a8e6752934cadc43394054214
ekkymulia <ekky.mulialasardi@gmail.com>
2023-05-22 23:36:18 +0700

    springboot --force

---
+++ src/main/java/com/sushimayo/SushiMayoApplication.java
@@ -0,0 +1,13 @@
+package com.sushimayo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushiMayoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SushiMayoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sushimayo/SushiMayoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sushimayo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SushiMayoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
