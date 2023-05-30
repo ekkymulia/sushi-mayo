b9045db1e77acab9503330ea785e9774abdacd33
unknown <luthfidika31@gmail.com>
2023-05-30 23:39:12 +0700

    co

--- src/main/java/com/sushimayo/controllers/publik/ShopController.java
+++ src/main/java/com/sushimayo/controllers/publik/ShopController.java
@@ -6,10 +6,7 @@ import com.sushimayo.models.MenuModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/shop")
@@ -34,6 +31,7 @@ public class ShopController {
         return "shop.html";
     }
 
+    @CrossOrigin(origins="*")
     @GetMapping(value = "/getProductDetails", produces = "application/json")
     @ResponseBody
     public MenuModel getMenuDetails(@RequestParam("id") String menuId) {
